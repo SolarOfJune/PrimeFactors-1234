fcfe629111957c88cc9d2ff325d4d6665889d774
jungi.lee <dffbnm@gmail.com>
2023-07-05 15:13:20 +0900

    [feature] page1

--- PrimeFactor_Test/prime-factors_test.cpp
+++ PrimeFactor_Test/prime-factors_test.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
 #include "../PrimeFactor/prime-factors.cpp"
+#include <vector>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(PrimeFactors, Of1) {
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
 }
